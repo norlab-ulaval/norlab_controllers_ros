83f4a2fa7b826c637d5c82d8b925aacf9638479d
DomBaril <dominic.baril@usherbrooke.ca>
2022-11-09 14:26:57 -0500

    implemented multi-threaded spinner

--- norlab_controllers_ros/controller_node.py
+++ norlab_controllers_ros/controller_node.py
@@ -7,6 +7,8 @@ import numpy as np
 import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionServer
+from rclpy.callback_groups import ReentrantCallbackGroup, MutuallyExclusiveCallbackGroup
+from rclpy.executors import MultiThreadedExecutor, SingleThreadedExecutor
 
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
@@ -45,7 +47,11 @@ class ControllerNode(Node):
         self.state = np.zeros(6) # [x, y, z, roll, pitch, yaw]
         self.velocity = np.zeros(6) # [vx, vy, vz, v_roll, v_pitch, v_yaw]
 
-        self.rate = self.create_rate(20)
+        self.rate = self.create_rate(self.controller.rate)
+
+        self.waiting_for_path = True
+        self.loading_path = False
+        self.executing_path = False
     def quaternion_to_euler(self, w, x, y, z):
         sinr_cosp = 2 * (w * x + y * z)
         cosr_cosp = 1 - 2 * (x ** 2 + y ** 2)
@@ -85,10 +91,16 @@ class ControllerNode(Node):
         self.cmd_vel_msg.angular.y = 0.0
         self.cmd_vel_msg.angular.z = command_array[1]
 
+    def compute_then_publish_command(self):
+        command_vector = self.controller.compute_command_vector(self.state)
+        self.command_array_to_twist_msg(command_vector)
+        self.cmd_publisher_.publish(self.cmd_vel_msg)
+
     def follow_path_callback(self, path_goal_handle):
         ## Importing all goal paths
         self.get_logger().info("Importing goal paths...")
         goal_paths = path_goal_handle.request.path.paths
+        self.path_goal_handle = path_goal_handle
         self.goal_paths_list = []
         for current_path in goal_paths:
             current_path_length = len(current_path.poses)
@@ -105,42 +117,70 @@ class ControllerNode(Node):
             current_path_object = Path(current_path_array)
             current_path_object.compute_metrics()
             self.goal_paths_list.append(current_path_object)
-        number_of_goal_paths = len(self.goal_paths_list)
-        self.get_logger().info("Path import done, proceeding to executing " + str(number_of_goal_paths) + " path(s)...")
+        self.number_of_goal_paths = len(self.goal_paths_list)
+        self.get_logger().info("Path import done, proceeding to executing " + str(self.number_of_goal_paths) + " path(s)...")
 
         ## execute paths one by one
-        for i in range(0, number_of_goal_paths):
+        for i in range(0, self.number_of_goal_paths):
             # load all goal paths in sequence
-            self.get_logger().info("Executing path " + str(i+1) + " of " + str(number_of_goal_paths))
+            self.get_logger().info("Executing path " + str(i + 1) + " of " + str(self.number_of_goal_paths))
             self.controller.update_path(self.goal_paths_list[i])
             # print(self.controller.path.poses)
             # while loop to repeat a single goal path
             while self.controller.distance_to_goal >= self.controller.goal_tolerance:
-                print(self.state)
-                command_vector = self.controller.compute_command_vector(self.state)
-                self.command_array_to_twist_msg(command_vector)
-                self.cmd_publisher_.publish(self.cmd_vel_msg)
-                print("a")
+                self.get_logger().info(self.state)
+                self.compute_then_publish_command()
                 self.rate.sleep()
-                print("b")
 
         ## return completed path to action client
         path_goal_handle.succeed()
         paths_result = FollowPath.Result()
         result_status = std_msgs.msg.UInt32()
-        result_status.data = 1 # 1 for success
+        result_status.data = 1  # 1 for success
         paths_result.result_status = result_status
         return paths_result
 
+    def follow_path_spinner(self):
+        if self.executing_path:
+            self.compute_then_publish_command()
+            if self.controller.distance_to_goal <= self.controller.goal_tolerance:
+                self.path_id += 1
+                if self.path_id >= self.number_of_goal_paths:
+                    self.get_logger().info("SUCCESS")
+                    ## return completed path to action client
+                    self.path_goal_handle.set_succeeded()
+                    paths_result = FollowPath.Result()
+                    result_status = std_msgs.msg.UInt32()
+                    result_status.data = 1  # 1 for success
+                    paths_result.result_status = result_status
+                    return paths_result
+                    self.executing_path = False
+                    return None
+                self.get_logger().info("Executing path " + str(self.path_id+1) + " of " + str(self.number_of_goal_paths))
+                self.controller.update_path(self.goal_paths_list[self.path_id])
+
+            else:
+                return None
+
 def main(args=None):
+    # initialize the ROS communication
     rclpy.init(args=args)
 
-    controller_node = ControllerNode()
+    try:
+        # declare the node constructor
+        controller_node = ControllerNode()
+        executor = MultiThreadedExecutor()
+        executor.add_node(controller_node)
 
-    rclpy.spin(controller_node)
-
-    controller_node.destroy_node()
-    rclpy.shutdown()
+        try:
+            # pause the program execution, waits for a request to kill the node (ctrl+c)
+            executor.spin()
+        finally:
+            executor.shutdown()
+            controller_node.destroy_node()
+    finally:
+        # shutdown the ROS communication
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
